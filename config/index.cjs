4180b43f257ea073a7b836f04090087534f07a24
coolorfulpork <60846839+coolorfulpork@users.noreply.github.com>
2024-07-22 18:42:20 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -7,13 +7,14 @@
  */
 const USER_CONFIG = {
   
-USE_PASSAGE: 'push-deer',
+// 请将以下代码按照图示粘贴到对应位置
+  USE_PASSAGE: 'push-deer',
   
   // 使用微信测试号：公众号APP_ID
-  APP_ID: 'PDU31122T6eAXjPtmLSyl7UZ4XZ4o8BA6NxH6X0dw',
+  APP_ID: '',
 
   // 使用微信测试号：公众号APP_SECRET
-  APP_SECRET: '0001',
+  APP_SECRET: '',
 
   PROVINCE: '浙江',
   CITY: '萧山',
@@ -23,9 +24,9 @@ USE_PASSAGE: 'push-deer',
       // 想要发送的人的名字
       name: '乖妹妹',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'PDU31122TM5HNnE5Hro50mNVo3dpzS1qUUzffbjCW',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '',
+      useTemplateId: '0001',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '07-31',
       festivals: [
